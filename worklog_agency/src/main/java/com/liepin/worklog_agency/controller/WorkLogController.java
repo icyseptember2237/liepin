92f048d396b35f8e97930fd64014f6d585cb270d
howhich <2210298380@qq.com>
2023-10-26 21:05:02 +0800

    fix 修改Controller 修改TestController为WorkLogController

--- worklog_agency/src/main/java/com/liepin/worklog_agency/controller/WorkLogController.java
+++ worklog_agency/src/main/java/com/liepin/worklog_agency/controller/WorkLogController.java
@@ -1,13 +1,8 @@
 package com.liepin.worklog_agency.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-    @GetMapping("/tjh")
-    public String hello(){
-        return "hello";
-    }
+public class WorkLogController {
 }
