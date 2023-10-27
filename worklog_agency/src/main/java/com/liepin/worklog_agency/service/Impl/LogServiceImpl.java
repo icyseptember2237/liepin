d6b34cc56a6302bcb22fb7fdbf1e24ae80b7b565
howhich <2210298380@qq.com>
2023-10-27 10:03:37 +0800

    feat LogMapper和Service新增 LogMapper和Service新增

--- worklog_agency/src/main/java/com/liepin/worklog_agency/service/Impl/LogServiceImpl.java
+++ worklog_agency/src/main/java/com/liepin/worklog_agency/service/Impl/LogServiceImpl.java
@@ -1,9 +1,18 @@
 package com.liepin.worklog_agency.service.Impl;
 
+import com.liepin.common.constant.classes.Result;
+import com.liepin.worklog_agency.mapper.LogMapper;
 import com.liepin.worklog_agency.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogServiceImpl implements LogService {
-
+    @Autowired
+    private LogMapper logMapper;
+    @Override
+    public Result getWorkLog(String loginId) {
+        String WorkLogId = logMapper.getWorkLog(loginId);
+        return Result.success();
+    }
 }
--- worklog_agency/src/main/java/com/liepin/worklog_agency/service/LogService.java
+++ worklog_agency/src/main/java/com/liepin/worklog_agency/service/LogService.java
@@ -1,5 +1,8 @@
 package com.liepin.worklog_agency.service;
 
+import com.liepin.common.constant.classes.Result;
+
 public interface LogService {
 
+    Result getWorkLog(String loginId);
 }
