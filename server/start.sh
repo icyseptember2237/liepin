d8490184dd9aeac3e7229b03c95b31b951780ded
September22 <1242664565@qq.com>
2023-12-16 16:48:36 +0800

    fix 自动部署脚本

--- server/start.sh
+++ server/start.sh
@@ -3,6 +3,8 @@
 mv /root/.jenkins/workspace/liepin/web/target/web-1.0.0.jar /root/project
 cd /root/project || return
 source /etc/profile
+echo 'JAVA_HOME is $JAVA_HOME'
+echo 'PATH is $PATH'
 nohup /jdk17/jdk-17.0.9/bin/java -jar -Xmx1024M -Xms256M  web-1.0.0.jar --server.port=8080 >/root/project/log.log 2>&1 &
 pid=`ps -ef | grep web | grep 'java -jar' | grep -v 'nohup' | awk '{printf $2}'`
 if [ -z $pid ];
