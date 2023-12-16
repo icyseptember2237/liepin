6ab00deeb02a12fc3ec46411d5a629cfa6d56faa
September22 <1242664565@qq.com>
2023-12-16 15:42:14 +0800

    feat 自动构建脚本

---
+++ server/x.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+appname=$1
+
+# 获取正在运行的 jar 包的 pid
+pid=`ps -ef | grep $appname | grep 'java -jar' | awk '{printf $2}'`
+
+#空值判断
+if [ -z $pid ];
+	then
+		echo "$appname not find"
+	else
+		# 结束进程 -9 无条件结束
+		kill -9 $pid
+		check=`ps -ef | grep -w $pid | grep java`
+		if [ -z $check ];
+			then
+				echo "Failed to end $appname pid:$pid"
+			else
+				echo "$appname is killed"
+		fi
+fi
